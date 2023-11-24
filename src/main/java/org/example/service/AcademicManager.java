a2433f69923bf2223e1192939960fa75850378bd
Daniela <danielapb78@gmail.com>
2023-11-24 18:15:12 -0500

    creacion de la clase Student Course y AcademicManager

---
+++ src/main/java/org/example/service/AcademicManager.java
@@ -0,0 +1,15 @@
+package org.example.service;
+
+import org.example.entity.Course;
+import org.example.entity.Student;
+
+import java.util.HashMap;
+import java.util.List;
+
+public class AcademicManager {
+
+    private List <Student> studentList;
+    private List <Course> courseList;
+    HashMap<String, List<String>> studentsAndCourses = new HashMap<>();
+
+}
