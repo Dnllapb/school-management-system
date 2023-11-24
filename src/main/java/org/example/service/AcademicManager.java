6d0f8c79c5051571fad52562efe706874a012b1b
Daniela <danielapb78@gmail.com>
2023-11-24 18:40:08 -0500

    creacion de la interfaz  ServicesAcademicsI con sus metodos

--- src/main/java/org/example/service/AcademicManager.java
+++ src/main/java/org/example/service/AcademicManager.java
@@ -2,14 +2,34 @@ package org.example.service;
 
 import org.example.entity.Course;
 import org.example.entity.Student;
+import org.example.interfaces.ServicesAcademicsI;
 
 import java.util.HashMap;
 import java.util.List;
 
-public class AcademicManager {
+public class AcademicManager implements ServicesAcademicsI {
 
     private List <Student> studentList;
     private List <Course> courseList;
     HashMap<String, List<String>> studentsAndCourses = new HashMap<>();
 
+    @Override
+    public void enrolledStudent(Student student) {
+
+    }
+
+    @Override
+    public void addCourse(Course course) {
+
+    }
+
+    @Override
+    public void enrolledStudentCourse(Student student, int idCourse) {
+
+    }
+
+    @Override
+    public void unsubscribe(int idStudent, int idCourse) {
+
+    }
 }
