5f6089fe6bab676cb757e459069e71e680254862
Daniela <danielapb78@gmail.com>
2023-11-24 19:54:44 -0500

    implementando la herencia de persona a estudiante

---
+++ src/main/java/org/example/entity/Person.java
@@ -0,0 +1,58 @@
+package org.example.entity;
+
+public abstract class Person {
+
+    private Integer id;
+    private String name;
+    private String last_name;
+    private String birthday;
+
+    public Person(Integer id, String name, String last_name, String birthday) {
+        this.id = id;
+        this.name = name;
+        this.last_name = last_name;
+        this.birthday = birthday;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+
+    public String getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(String birthday) {
+        this.birthday = birthday;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", last_name='" + last_name + '\'' +
+                ", birthday='" + birthday + '\'' +
+                '}';
+    }
+}
--- src/main/java/org/example/entity/Student.java
+++ src/main/java/org/example/entity/Student.java
@@ -1,70 +1,12 @@
 package org.example.entity;
 
-public class Student {
-
-    private Integer id_student;
-    private String name_student;
-    private String last_name;
-    private String birthday;
-
+public class Student extends Person{
     private String status;
+    public Student(Integer id, String name, String last_name, String birthday) {
+        super(id, name, last_name, birthday);
 
-    public Student(Integer id_student, String name_student, String last_name, String birthday, String status) {
-        this.id_student = id_student;
-        this.name_student = name_student;
-        this.last_name = last_name;
-        this.birthday = birthday;
-        this.status = status;
     }
 
-    public Integer getId_student() {
-        return id_student;
-    }
 
-    public void setId_student(Integer id_student) {
-        this.id_student = id_student;
-    }
 
-    public String getName_student() {
-        return name_student;
-    }
-
-    public void setName_student(String name_student) {
-        this.name_student = name_student;
-    }
-
-    public String getLast_name() {
-        return last_name;
-    }
-
-    public void setLast_name(String last_name) {
-        this.last_name = last_name;
-    }
-
-    public String getBirthday() {
-        return birthday;
-    }
-
-    public void setBirthday(String birthday) {
-        this.birthday = birthday;
-    }
-
-    public String getStatus() {
-        return status;
-    }
-
-    public void setStatus(String status) {
-        this.status = status;
-    }
-
-    @Override
-    public String toString() {
-        return "Student{" +
-                "id_student=" + id_student +
-                ", name_student='" + name_student + '\'' +
-                ", last_name='" + last_name + '\'' +
-                ", birthday='" + birthday + '\'' +
-                ", status='" + status + '\'' +
-                '}';
-    }
 }
